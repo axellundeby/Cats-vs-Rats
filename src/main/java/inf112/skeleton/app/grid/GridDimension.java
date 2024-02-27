c35251c4a5cd4547dd9fa2d33cab36a35d31ff63
Theodor <theodornm@gmail.com>
2024-02-27 14:23:11 +0100

    Added Grid skeleton

---
+++ src/main/java/inf112/skeleton/app/grid/GridDimension.java
@@ -0,0 +1,14 @@
+package no.uib.inf101.grid;
+
+public interface GridDimension {
+
+  /** 
+   * @return Number of rows in the grid
+   */
+  int rows();
+
+  /**
+   * @return Number of columns in the grid 
+   */
+  int cols();
+}
