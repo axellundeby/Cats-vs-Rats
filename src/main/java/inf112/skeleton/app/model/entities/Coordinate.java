e5967a5eccf3380d1b1635a88dcca5bcd0d0700a
Theodor <theodornm@gmail.com>
2024-03-07 19:37:01 +0100

    Removed unused for oblig2

--- src/main/java/inf112/skeleton/app/model/entities/Coordinate.java
+++
@@ -1,8 +0,0 @@
-package inf112.skeleton.app.model.entities;
-
-public record Coordinate(int x, int y) {
-    @Override
-    public String toString(){
-        return "Coordinate x: " + x() + ", y: " + y();
-    }
-}
