92b92fc9bec320cce1b16b46a1fef09afec26238
Jacob Foss <jafos7176@uib.no>
2024-03-11 12:46:58 +0100

    Fixing FreezeCat, done

---
+++ src/main/java/inf112/skeleton/app/model/entities/cat/FreezeCat.java
@@ -0,0 +1,26 @@
+package inf112.skeleton.app.model.entities.cat;
+
+import java.util.LinkedList;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+import inf112.skeleton.app.model.entities.rat.Rat;
+
+public class FreezeCat extends Cat{
+
+    public FreezeCat() {
+        super(1, 100, new Texture(Gdx.files.internal("freezeCat.png")));
+        System.out.println("mjau");
+    }
+
+    @Override
+    public void attack(LinkedList<Rat> rats) {
+        for (Rat rat : rats) {
+            rat.freeze();
+            System.out.println("Rat is frozen");
+            //må unfreeze etter x sekunder
+        }
+    }
+    
+}
