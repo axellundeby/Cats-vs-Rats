c79d7997e5370bbcf893a071541186ed59aa8cb7
git <git>
2024-04-09 22:02:04 +0200

    rat classes added

--- src/main/java/inf112/skeleton/app/model/entities/rat/BossRat.java
+++ src/main/java/inf112/skeleton/app/model/entities/rat/BossRat.java
@@ -3,9 +3,9 @@ package inf112.skeleton.app.model.entities.rat;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
-public class BasicRat extends Rat {
+public class BossRat extends Rat{
     Texture texture = new Texture(Gdx.files.internal("rat1.png"));
-    public BasicRat() {
+    public BossRat() {
        super(70, 2, new Texture(Gdx.files.internal("rat1.png")),200,1);
         //super(health, 2, texture, bounty, points); 
     }
--- src/main/java/inf112/skeleton/app/model/entities/rat/FastRat.java
+++ src/main/java/inf112/skeleton/app/model/entities/rat/FastRat.java
@@ -1,7 +1,10 @@
 package inf112.skeleton.app.model.entities.rat;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 public class FastRat extends Rat {
     public FastRat() {
-        super(100, 10,null,1,100);
+        super(50, 4, new Texture(Gdx.files.internal("fast_rat.png")), 300, 2);
     }
 }
