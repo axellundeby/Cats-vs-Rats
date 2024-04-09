35efa9231d5ce44ec65bb1eaedd90f6159ab4833
Theodor <theodornm@gmail.com>
2024-04-09 10:04:01 +0200

    Begun model tests

--- src/test/java/inf112/skeleton/app/model/ModelTest.java
+++ src/test/java/inf112/skeleton/app/model/ModelTest.java
@@ -2,6 +2,9 @@ package inf112.skeleton.app.model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -11,49 +14,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 
+import inf112.skeleton.app.model.entities.cat.BasicCat;
+import inf112.skeleton.app.model.entities.cat.Cat;
+import inf112.skeleton.app.model.entities.cat.FreezeCat;
+import inf112.skeleton.app.model.entities.rat.BasicRat;
+import inf112.skeleton.app.model.entities.rat.Rat;
+
 public class ModelTest {
+    static SkadedyrModel model;
     @BeforeAll
 	static void setUpBeforeAll() {
-        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
-        ApplicationListener listener = new ApplicationListener() {
+        model = new SkadedyrModel();
+    }
 
-            @Override
-            public void create() {
-                // TODO Auto-generated method stub
+    @BeforeEach
+    void beforeEach(){}
 
-            }
-
-            @Override
-            public void resize(int width, int height) {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void render() {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void pause() {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void resume() {
-                // TODO Auto-generated method stub
-
-            }
-
-            @Override
-            public void dispose() {
-                // TODO Auto-generated method stub
-
-            }
-        };
-   new HeadlessApplication(listener, config);
-
+    @Test
+    public void attackQueueTest(){
+        Cat cat1 = new BasicCat();
+        Cat cat2 = new FreezeCat();
+        model.addCat(cat1);
+        model.addCat(cat2);
+        Rat rat1 = new BasicRat();
+        HashMap<Cat, LinkedList<Rat>> attackMap = model.attackQueueForEachCat();
     }
 }
