354bdddb0a88ebec6c24f54b4731cecec2c0f577
Jonathan.Christensen <jonathan.christensen@student.uib.no>
2024-04-10 14:18:09 +0200

    fixed rotation

--- src/main/java/inf112/skeleton/app/model/entities/rat/BasicRat.java
+++ src/main/java/inf112/skeleton/app/model/entities/rat/BasicRat.java
@@ -7,6 +7,7 @@ public class BasicRat extends Rat {
     Texture defaultTexture = new Texture(Gdx.files.internal("Spill_Rotte1.png"));
     Texture freezeTexture = new Texture(Gdx.files.internal("Spill_Rotte1_freezed.png"));
     public BasicRat() {
-        super(70, 2, new Texture(Gdx.files.internal("rats/Spill_Rotte1.png")),200,1);
+        super(70, 2, new Texture(Gdx.files.internal("rats/Spill_Rotte1.png")),200,1,new Texture(Gdx.files.internal("Spill_Rotte1_freezed.png")),40);
     }
 }
+
