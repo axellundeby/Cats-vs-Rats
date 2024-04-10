2a3f9ce01dfbea43522ee9e8873bf1f0ecc089ae
Theodor <theodornm@gmail.com>
2024-04-10 11:16:16 +0200

    Minor cleanup

--- src/test/java/inf112/skeleton/app/model/ModelTest.java
+++ src/test/java/inf112/skeleton/app/model/ModelTest.java
@@ -7,19 +7,13 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.math.Vector2;
 
-import inf112.skeleton.app.main.SkadedyrGame;
 import inf112.skeleton.app.model.entities.cat.BasicCat;
 import inf112.skeleton.app.model.entities.cat.Cat;
-import inf112.skeleton.app.model.entities.cat.FreezeCat;
 import inf112.skeleton.app.model.entities.rat.BasicRat;
 import inf112.skeleton.app.model.entities.rat.Rat;
 
@@ -187,9 +181,5 @@ public class ModelTest {
     }
 
 
-	@CsvSource(value = { "1,1,2", "1,2,3", "2,3,5", "3,5,8", "5,8,13", "8,13,21" })
-	@ParameterizedTest(name = "{0}+{1} == {2}")
-	void addTest(int a, int b, int c) {
-		assertEquals(c, a + b);
-	}
+	
 }
