fd6e21cfeecf3c64617f5f3f758b05d81b655e50
Jacob Foss <jafos7176@uib.no>
2024-04-10 16:12:21 +0200

    Fixed the image path for basicrat

--- src/main/java/inf112/skeleton/app/model/entities/rat/BasicRat.java
+++ src/main/java/inf112/skeleton/app/model/entities/rat/BasicRat.java
@@ -4,10 +4,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 public class BasicRat extends Rat {
-    Texture defaultTexture = new Texture(Gdx.files.internal("Spill_Rotte1.png"));
-    Texture freezeTexture = new Texture(Gdx.files.internal("Spill_Rotte1_freezed.png"));
+    Texture defaultTexture = new Texture(Gdx.files.internal("rats/Spill_Rotte1.png"));
+    Texture freezeTexture = new Texture(Gdx.files.internal("rats/Spill_Rotte1_freezed.png"));
     public BasicRat() {
-        super(70, 2, new Texture(Gdx.files.internal("rats/Spill_Rotte1.png")),200,1,new Texture(Gdx.files.internal("Spill_Rotte1_freezed.png")),40);
+        super(70, 2, new Texture(Gdx.files.internal("rats/Spill_Rotte1.png")),200,1,new Texture(Gdx.files.internal("rats/Spill_Rotte1_freezed.png")),40);
     }
 }
 
