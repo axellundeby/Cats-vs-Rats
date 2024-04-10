e9a7beef38a14f16f1958ba58a97afafa370f5a9
Theodor <theodornm@gmail.com>
2024-04-10 15:14:27 +0200

    Created testclasses or rats

---
+++ src/test/java/inf112/skeleton/app/model/entities/rat/LabRat.java
@@ -0,0 +1,11 @@
+package inf112.skeleton.app.model.entities.rat;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class LabRat extends Rat{
+
+    public LabRat(int health, int speed, Texture texture, Integer bounty, Integer points) {
+        super(health, speed, texture, texture, texture, bounty, points);
+    }
+    
+}
