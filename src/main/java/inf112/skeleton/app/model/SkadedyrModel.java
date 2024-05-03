30979ce4c340bd7b5608577bc94a0b8004f4a0af
Axel Lundeby <example@ehco.uib.no>
2024-05-03 12:42:29 +0200

    rat factory test fixed

--- src/main/java/inf112/skeleton/app/model/SkadedyrModel.java
+++ src/main/java/inf112/skeleton/app/model/SkadedyrModel.java
@@ -21,8 +21,8 @@ public class SkadedyrModel implements ISkadedyrModel {
     private RatFactory ratFactory;
     private int lives;
     private int money;
-    private int points = 0;
-    private int level = 0;
+    private int points;
+    private int level;
     private boolean isPaused;
     private float intervalSeconds = (float) 0.05;
     private CatMenu catMenu;
@@ -59,12 +59,14 @@ public class SkadedyrModel implements ISkadedyrModel {
         cats.clear();
         aliveRats.clear();
         newRats.clear();
-        ratFactory.resetRatFactory();
+        ratFactory.removeRats();
         money = 10000;
         lives = 5;
         isPaused = true;
         intervalSeconds = 0.05f;
         speedUp = false;
+        level = 0;
+        points = 0;
     }
 
     private void removeAllRats() {
